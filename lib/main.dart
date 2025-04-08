a9ee144d92e0884bda704ac896d07316e27f7910
DIEGO_ALI_LEDEZMA_CARBAJAL <a.22308051281229@cbtis128.edu.mx>
2025-04-08 14:54:08 +0000

    g

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,130 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(WineApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class WineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Información del Vino',
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Colors.pink[50],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WineInfoPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class WineInfoPage extends StatefulWidget {
+  @override
+  _WineInfoPageState createState() => _WineInfoPageState();
+}
+
+class _WineInfoPageState extends State<WineInfoPage> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _isExpanded = !_isExpanded;
+            });
+          },
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(20)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            elevation: MaterialStateProperty.all<double>(5),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/vino5.png'), // Cambiado a vino3.png
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeInOutQuad,
+              width: _isExpanded ? 400 : 350,
+              child: _isExpanded
+                  ? Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Vino Tinto Malbec',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.pink[800],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        _buildAnimatedInfoRow(
+                            'Variedad:', 'Malbec', Colors.pink[400]!),
+                        _buildAnimatedInfoRow(
+                            'Origen:', 'Mendoza, Argentina', Colors.pink[500]!),
+                        _buildAnimatedInfoRow(
+                            'Añada:', '2020', Colors.pink[600]!),
+                        _buildAnimatedInfoRow(
+                            'Alcohol:', '14% vol.', Colors.pink[700]!),
+                        _buildAnimatedInfoRow(
+                            'Azúcares:', '2 g/L', Colors.pink[800]!),
+                        _buildAnimatedInfoRow(
+                            'Calorías:', '70 kcal/100ml', Colors.pink[900]!),
+                      ],
+                    )
+                  : Center(
+                      child: Text(
+                        'Presióname para descubrir',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.pink[800],
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Colors.pink[100]!,
+                              offset: Offset(5.0, 5.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedInfoRow(String label, String value, Color color) {
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 300),
+      opacity: _isExpanded ? 1.0 : 0.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: RichText(
+          text: TextSpan(
+            style: TextStyle(fontSize: 16, color: Colors.pink[900]!),
+            children: <TextSpan>[
+              TextSpan(
+                text: label,
+                style: TextStyle(fontWeight: FontWeight.bold, color: color),
+              ),
+              TextSpan(text: ' $value'),
+            ],
+          ),
         ),
       ),
     );
